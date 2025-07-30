2aa75fe72d722191ea15c3dc83231f88bd56cbe8
goblin-A1 <onyibedennis@gmail.com>
2025-07-30 14:28:08 +0100

    first commit

---
+++ personalized_greeting_app.py
@@ -0,0 +1,16 @@
+name = input('what is your name? ')
+color = input('what is your favourite color? ')
+print(f'Hello {name}, your favorite color {color} is perfect')
+age = input('how old are you? ')
+married = input('are you married? ')
+print(f'Are you sure a {age} year old is not too old? ')
+
+
+# it took me time to get this right 
+# my mistake was that i didn't use the f-string correctly 
+# I never even knew what an f-string was until now
+
+# key things to note: 
+# 1. this is a tuple ()
+# 2. the input function takes a string as an argument
+# 3. print(f'hello[name]...') is the correct way to print a tuple variable
